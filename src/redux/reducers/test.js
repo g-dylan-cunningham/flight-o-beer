4f3227e0e157de9fcdb1c577de2295c2b0bf764f
Dylan Cunningham <gdylanc@gmail.com>
2018-02-21 20:24:51 -0700

    adds reducer to handle fetchAllSymbols

--- src/redux/reducers/test.js
+++ src/redux/reducers/test.js
@@ -1,11 +1,20 @@
-let initState = {test: true}
+let initState = {}
 
 export default (state = initState, action) => {
     switch (action.type) {
-        case "TEST":
+        case "FETCH_ALL_SYMBOLS_FULFILLED":
+            return {
+                ...state,
+                symbols: action.payload.data
+            };
+        case "FETCH_ALL_SYMBOLS_PENDING":
             return {
                 ...state
-            }
+            };
+        case "FETCH_ALL_SYMBOLS_REJECTED":
+            return {
+                ...state
+            };
         default:
             return {
                 ...state
