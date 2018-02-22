d5692d8fe721b25588da6237de79c7718bf9c919
Dylan Cunningham <gdylanc@gmail.com>
2018-02-21 20:25:19 -0700

    imports fetchAllSymbols reducer into combineReducers function

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import test from './test';
+import fetchAllSymbols from './test';
 
 export default combineReducers({
-    test: test
+    symbols: fetchAllSymbols,
 })
